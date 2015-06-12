31e7bf4a0482096956944818036892f798060575
LozenPoi <zluo@umich.edu>
2015-06-11 21:01:13 -0400

    bug fixed

--- bounded_noise.m
+++ bounded_noise.m
@@ -1,11 +1,11 @@
 function noise=bounded_noise(a,k,T,flag)
 
 % This function creats vector valued (scalar valued if the dimension of the
-%   noise vectors is 1) noise whose a1-a2 norm is bounded by k.
+%   noise is 1) noise whose norm is bounded by k.
 %
 % Inputs:
-%   a -- an n-by-1 vector corresponding to the norm types
-%   k -- an n-by-1 vector corresponding to the upper bounds
+%   a -- an n-by-1 vector corresponding to the norm types (for n dimension)
+%   k -- an n-by-1 vector corresponding to the upper bounds (for n dimension)
 %   T -- time horizon
 %   flag -- set to 1 if the initial seed needs to be reset to a fixed value
 % Outputs:
@@ -16,7 +16,13 @@ function noise=bounded_noise(a,k,T,flag)
 %   noise=bounded_noise(a,k,T,flag);
 %
 % Author: MI4Hybrid
-% Date of Last Modification: May 22nd, 2015
+% Date: May 22nd, 2015
+
+% Change a scalar bound to a vector having the same entries.
+if(length(k)==1&&length(a)~=1)
+    k=zeros(length(a),1)+k;
+    warning('Input bound is a scalar, converted to a vector having the same entries.');
+end
 
 % Find the dimension of noise.
 if(length(a)~=length(k))
@@ -44,8 +50,9 @@ end
 noise=zeros(n,T);
 for i=1:n
     if(a(i)>=1&&a(i)~=inf)
-        element_bound=nthroot(k(i)^a(i)/T,a(i));
-        noise(i,:)=(rand(1,T)-0.5)*2*element_bound;
+        noise(i,:)=(rand(1,T)-0.5)*2;
+        noise_norm=norm(noise(i,:),a(i));
+        noise(i,:)=noise(i,:)*k(i)/noise_norm;
     else
         noise(i,:)=(rand(1,T)-0.5)*2*k(i);
     end
