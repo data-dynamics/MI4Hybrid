0ad84c75f2a2f2cae4cbfe5a6cb152b5cf62e284
LozenPoi <zluo@umich.edu>
2015-07-07 00:50:36 -0400

    Function finished and tested

--- Functions/InvalidationARX.m
+++ Functions/InvalidationARX.m
@@ -1,7 +1,9 @@
-function result=InvalidationARX(sys,input,output)
+function result=InvalidationARX(sys,input,output,pn_bound,mn_bound)
 
 % This function applies the invalidation algorithm for ARX models based on
-% the system input and output.
+% the system input and output. This invlidation algorithm is made to check
+% the feasibility of a linear equation in the form of M*x=b where x is the
+% variable vector constructed using noise vectors.
 %
 % Input/parameters:
 %   sys -- the system model
@@ -13,62 +15,69 @@ function result=InvalidationARX(sys,input,output)
 % Author: MI4Hybrid
 % Date: June 22nd, 2015
 
-switch sys.mark
-    case 'arx'
-        result=InvalidationNARX(sys,input,output);
-    case 'swarx'
-        result=InvalidationSARX(sys,input,output);
+% Check if the system model is valid for this function.
+if(strcmp(sys.mark,'arx')~=1)
+    error('The input system model must be a non-switched ARX model.');
+end
+% Check if the input and output are consistent.
+if(length(input)~=length(output))
+    error('The input and output are not consistent.');
+end
+% Check if the input is consistent with with the model.
+if(size(input,1)~=size(sys.mode.C,2))
+    error('The input is not consistent with the model.');
+end
+% Check if the output is consistent with the model.
+if(size(output,1)~=size(sys.mode.A,2))
+    error('The output is not consistent with the model.');
 end
 
-end
+% Obtain the system model information.
+T=size(input,2); % time horizon
+n_y=size(sys.mode.A,1); % output dimension
+degree=size(sys.mode.A,3); % degree of the system
 
-%--------------Invalidate Non-switched ARX Model--------------
-function result=InvalidationNARX(sys,input,output)
-
-% This invlidation algorithm checks the feasibility of a linear equation in
-% the form of M*x=b where x is the variable vector.
-
-% It is possible that the input and output have different lengths since the
-% function "simulates" will pad zeros to the input if the length of input
-% is less than the specified time horizon T.
-if(length(input)<length(output))
-    %pad zero?
-elseif(length(input)>length(output))
-    %cut off?
-end
-
-% Construct the matrix M.
-M1=eye(n_y*(T-degree));
+% Construct the matrix M using system parameters.
+M1=kron(eye(T-degree),sys.Ep);
 M2=zeros(n_y*(T-degree),n_y*T);
 num_loop=T-degree;
 buffer=zeros(n_y,n_y*(degree+1));
 for i=1:degree
-    buffer(:,n_y*(i-1)+1:n_y*i)=sys.mode.A(:,:,degree-i+1)*A_factor;
+    buffer(:,n_y*(i-1)+1:n_y*i)=sys.mode.A(:,:,degree-i+1)*sys.Em;
 end
-buffer(:,n_y*degree+1:n_y*(degree+1))=-eye(n_y);
+buffer(:,n_y*degree+1:n_y*(degree+1))=-sys.Em;
 for i=1:num_loop
     M2(1+n_y*(i-1):n_y*i,1+n_y*(i-1):(degree+i)*n_y)=buffer;
 end
 M=[-M1 M2];
 
-% Construc the vector b.
+% Construct the vector b using I/O data as well as the system parameters.
+b=zeros(n_y,T-degree);
 for i=1:degree
-    b=b+sys.mode.A(:,:,i)*A_factor*y(:,degree+1-i:T-i)+sys.mode.C(:,:,i)*C_factor*input(:,degree+1-i:T-i);
+    b=b+sys.mode.A(:,:,i)*output(:,degree+1-i:T-i)+...
+        sys.mode.C(:,:,i)*input(:,degree+1-i:T-i);
 end
-F=bsxfun(@plus,zeros(n_y,num_loop),f);
-b=b-y(:,degree+1:T)+F;
+F=bsxfun(@plus,zeros(n_y,num_loop),sys.f);
+b=b-output(:,degree+1:T)+F;
 b=reshape(b,[],1);
 
-
 % Check the feasibility subject to the noise bound.
-
-
+n=sdpvar((T-degree)*n_y+T*n_y,1);
+constraints=[M*n==b];
+for i=1:n_y
+    constraints=[constraints,...
+        norm(n(i:n_y:n_y*(T-degree)),sys.pn_norm(i))<=pn_bound(i)];
 end
-
-%--------------Invalidate Switched ARX Model--------------
-function result=InvalidationSARX(sys,input,output)
-
-% Dummy implementation
-result=true;
+for i=1:n_y
+    constraints=[constraints,...
+        norm(n(n_y*(T-degree)+i:n_y:end),sys.mn_norm(i))<=mn_bound(i)];
+end
+options=sdpsettings('verbose',0,'solver','mosek');
+solution=optimize(constraints,[],options);
+if(solution.problem==0)
+    result=true;
+elseif(solution.problem==1)
+    result=false;
+end
 
 end
