2a4feec51615d854ba78e96c36603884c30f5d1d
LozenPoi <zluo@umich.edu>
2015-07-10 16:54:18 -0400

    example model modified, better for testing

--- examples/SS_invalidation_example.m
+++ examples/SS_invalidation_example.m
@@ -3,39 +3,50 @@
 clear all
 addpath 'C:/Users/Administrator/Documents/MATLAB/Summer/MI4Hybrid2/lib'
 
-% A segwway model.
-A=[-1 0 0 0;0 -2 0 0;0 0 -3 0;0 0 0 -0.5];
-B=[0 0 0 0]';
-C=[0 -1 0 0;0 0 1 0];
-D=[0 0]';
-g=[0 0 0 0]';
-f=[0 0]';
+% A test model.
+A=[0.54 0.21 0.23;0.44 0.27 0.24;0.43 0.21 0.31];
+B=[0 1 0]';
+C=[1 0 0;0 1 0;0 0 1];
+D=[0 0 0]';
+g=[0 0 0]';
+f=[0 0 0]';
 
 % Define noise parameters
-pn_norm=[inf inf inf inf];
-mn_norm=[inf inf];
-pn_bound=[0 0 0 0];
-mn_bound=[0.3 0.5];
+pn_norm=[inf inf inf];
+mn_norm=[inf inf inf];
+pn_bound=[1 4 2];
+mn_bound=[5 4 1];
+
+Ep=eye(3);
+Em=eye(3);
+input_norm=inf;
+input_bound=inf;
+state_norm=[inf inf inf];
+state_bound=[1 1 1]*20;
 
 % Creat a system class.
-sys=StateSpace(A,B,C,D,g,f,pn_norm,mn_norm);
+sys=StateSpace(A,B,C,D,g,f,pn_norm,mn_norm,Ep,Em,input_norm,input_bound,state_norm,state_bound);
 
 % Creat an input sequence.
-T=100;
-input=ones(1,T)*0.5;
+T=500;
+input=ones(1,T);
 ini_cond=[];
 
-pn_bound_test=[0.2 0.3 0.1 0.4];
-mn_bound_test=[0.3 0.5];
+pn_bound_test=[1 4 2];
+mn_bound_test=[5 4 1];
+p=0.8;
+sys2=StateSpace(A*p,B*p,C*p,D*p,g*p,f*p,pn_norm,mn_norm,Ep,Em,input_norm,input_bound,state_norm,state_bound);
 
 % Apply invalidation algorithm with the simulation function.
 count=0;
-for i=1:5
+for i=1:20
     [output,~,~,~]=simulates(sys,input,T,ini_cond,pn_bound,mn_bound);
-    result=InvalidationSS(sys,input,output,pn_bound_test,mn_bound_test);
+    result=InvalidationSS(sys2,input,output,pn_bound_test,mn_bound_test);
     if(result==false)
         disp('invalidated');
         count=count+1;
+    else
+        disp('validated');
     end
 end
 
