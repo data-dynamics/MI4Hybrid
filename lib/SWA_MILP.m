925a1a2a24c04fecfa832632ce4daf6f85d8b2ed
LozenPoi <zluo@umich.edu>
2015-07-20 16:43:25 -0400

    SWA_MILP Finalized!

--- lib/SWA_MILP.m
+++ lib/SWA_MILP.m
@@ -1,31 +1,31 @@
-function [Decision, sol] = SWA_MILP(SYS,y,u,p,u_bound,x_bound,mn_bound, solver)
+function [Decision, sol] = SWA_MILP(SYS,input,output,mn_bound,...
+    input_bound,state_bound,solver)
 
-% This function implements MILP-based model invalidation approach for 
-% switched state-space models discussed in "Model Invalidation for Switched 
-% Affine Systems with Applications to Fault and Anomaly Detection" by 
+% This function implements MILP-based model invalidation approach for the
+% switched state-space models discussed in "Model Invalidation for Switched
+% Affine Systems with Applications to Fault and Anomaly Detection" by
 % F. Harirchi and N. Ozay.
 %
 % Preleminaries for this function are: YALMIP and (cplex or gurobi).
 %
 % Inputs/Parameters:
 %   SYS -- a user-defined system class (see StateSpace.m)
-%   y -- Output sequence
-%   u -- input sequence (an n_u-by-T matrix) where n_u is the dimension
+%   input -- input sequence (an n_i-by-T matrix) where n_i is the dimension
 %            of input and T is time horizon
-%   p -- a scalar denoting the type of the norm to be used to put 
-%         constraints on input.
-%   u_bound -- an n_u-by-1 vector specifying p-norm bound on input values, 
-%               where n_u is number of inputs.
-%   x_bound -- an n-by-1 vector specifying infinity norm bound on state 
-%               variables, where n is number of states.
+%   output -- output sequence (an n_y-by-T matrix) where n_y is the
+%             dimension of output and T is time horizon
 %   mn_bound -- an n_y-by-1 vector specifying the bound for process noise
-%                where n_y is the dimension of output.
-%   solver -- MILP solver to be used to solve the optimization problem.
+%               where n_y is the dimension of output
+%   input_bound -- an n_i-by-1 vector specifying p-norm bound on input
+%                  values where n_i is number of inputs
+%   state_bound -- an n-by-1 vector specifying infinity norm bound on state 
+%                  variables, where n is number of states
+%   solver -- MILP solver to be used to solve the optimization problem
 %            -- 'cplex': uses CPLEX solver from IBM (needs to be installed)
-%                 for more information on cplex see: "http://www-03.ibm.com
-%                 /software/products/en/ibmilogcpleoptistud"
+%                for more information on cplex see: "http://www-03.ibm.com
+%                /software/products/en/ibmilogcpleoptistud"
 %            -- 'gurobi': uses Gurobi as the solver, for more information
-%                          see "http://www.gurobi.com"
+%                see "http://www.gurobi.com"
 % Output:
 %   Decision -- the output is one of the following messages:
 %               - 'Input bounds are not satisfied'
@@ -34,20 +34,76 @@ function [Decision, sol] = SWA_MILP(SYS,y,u,p,u_bound,x_bound,mn_bound, solver)
 %   sol -- A solution structure that is the output of YALMIP
 %
 % Syntax:
-%   Decision = SWA_MILP(SYS,y,u,p,u_bound,x_bound,mn_bound, solver);
+%   Decision = SWA_MILP(SYS,input,output,mn_bound,input_bound,state_bound);
+%   Decision = SWA_MILP(SYS,input,output,mn_bound,input_bound,...
+%                       state_bound,solver);
 %
 % Author: MI4Hybrid
 % Date: July 13th, 2015
 
+%% Check if the system model is valid for this function.
+if(strcmp(SYS.mark,'ss')~=1&&strcmp(SYS.mark,'swss')~=1)
+    error('The system model must be an ARX model.');
+end
+
+%% Check if the input, output, and the model are consistent.
+if(length(input)~=length(output))
+    error('The input length and output length are not consistent.');
+end
+if(size(input,1)~=size(SYS.mode(1).B,2))
+    error('The input is not consistent with the model.');
+end
+if(size(output,1)~=size(SYS.mode(1).C,1))
+    error('The output is not consistent with the model.');
+end
 
 %% Input, States, Output dimensions and time horizon 
-[n_y N] = size(y);
-n_u = size(u);
-num_m = size(SYS.mode,2);   % number of modes
-n = size(SYS.mode(1).A,1);
+[n_y,T] = size(output); % output dimension and time horizon
+n_i = size(input); % input dimension
+n_mode = size(SYS.mode,2); % number of modes
+n = size(SYS.mode(1).A,1); % state dimension
+
+%% Use the default solver if it is not specified.
+if(nargin==6)
+    solver='cplex';
+end
+
+%% Set up default values for empty paramters.
+if(isempty(mn_bound))
+    mn_bound=zeros(n_y,1);
+end
+if(isempty(input_bound))
+    input_bound=zeros(n_i,1)+inf;
+end
+if(isempty(state_bound))
+    state_bound=zeros(n,1)+inf;
+end
+if(isempty(solver))
+    solver='cplex';
+end
+
+%% Convert scalars to vectors.
+if(length(mn_bound)==1&&n_y>1)
+    mn_bound=ones(n_y,1)*mn_bound;
+    warning(['Bound for measurement noise is a scalar, converted to'...
+        ' a vector with identical entries.']);
+end
+if(length(state_bound)==1&&n>1)
+    state_bound=ones(n,1)*state_bound;
+    warning(['State bound is a scalar, converted to a vector with '...
+        'identical entries.']);
+end
+
+%% Check the bounds.
+if(length(mn_bound)~=n_y||~isvector(mn_bound))
+    error('The number of bounds for measurement noise is not correct.');
+end
+if(length(state_bound)~=n||~isvector(state_bound))
+    error('The number of bounds for states is not correct.');
+end
 
 %% Initiate system modes
-for i = 1: num_m
+for i = 1: n_mode
     Mode(i).A = SYS.mode(i).A;
     Mode(i).B = SYS.mode(i).B;
     Mode(i).C = SYS.mode(i).C;
@@ -55,76 +111,67 @@ for i = 1: num_m
     Mode(i).f = SYS.mode(i).f;
     Mode(i).g = SYS.mode(i).g;
 end
-
-M = x_bound;
+M = state_bound;
 eps = mn_bound;
-U = u_bound;
-
 
 %% Checking input bounds
-for i = 1:n_u
-    u_norm(1,i) = norm(u(i,:),p);
+for i = 1:n_i
+    u_norm(1,i) = norm(input(i,:),SYS.input_norm(i));
 end
-if sum(u_norm > u_bound) >0
+if sum(u_norm > input_bound) >0
     Decision = 'Input bounds are not satisfied';
     return
 end
 
-
 %% Building the constraints
-for i = 1:num_m
+for i = 1:n_mode
     sys(i).g = [-Mode(i).C -ones(n_y,1)];
     sys(i).q = [-Mode(i).D ones(n_y,1)];
     sys(i).h = [-Mode(i).A eye(n)];
     sys(i).l = [-Mode(i).B];
 end
 
-s = binvar(N,num_m);
-e = sdpvar(num_m*N,n_y);
-X = sdpvar(num_m*N,2*n);
+s = binvar(T,n_mode);
+e = sdpvar(n_mode*T,n_y);
+X = sdpvar(n_mode*T,2*n);
 
 Constraint = [];
-for t = 1:N-1    % time index
-    
-    for sys_ind = 1: num_m  % mode index
-        
+for t = 1:T-1    % time index
+    for sys_ind = 1: n_mode  % mode index
         for j = 1:n_y   % constraints for output equation
             Constraint = [Constraint sys(sys_ind).g(j,:)*[X((t-1)* ...
-                num_m+sys_ind,1:n) e((t-1)*num_m+sys_ind,j)]'+ ...
-                s(t,sys_ind)*sys(sys_ind).q(j,:)*[u(:,t) y(j,t)]' ==0];
+                n_mode+sys_ind,1:n) e((t-1)*n_mode+sys_ind,j)]'+ ...
+                s(t,sys_ind)*sys(sys_ind).q(j,:)* ...
+                [input(:,t) output(j,t)]'==0];
         end
-        
         for state_ind = 1:n  % constraints for state equation
             Constraint = [Constraint sys(sys_ind).h(state_ind,:)* ...
-                X((t-1)*num_m+sys_ind,:)'+ ...
-                s(t,sys_ind)*sys(sys_ind).l(state_ind,:)*u(:,t)'==0];
-            Constraint = [Constraint norm(X((t-1)*num_m+sys_ind, ...
+                X((t-1)*n_mode+sys_ind,:)'+ ...
+                s(t,sys_ind)*sys(sys_ind).l(state_ind,:)*input(:,t)'==0];
+            Constraint = [Constraint norm(X((t-1)*n_mode+sys_ind, ...
                 state_ind),inf)<=M(state_ind)*s(t,sys_ind)];
         end
-        
-        
-        Constraint = [Constraint norm(e((t-1)*num_m+sys_ind,:),inf)<=...
+        Constraint = [Constraint norm(e((t-1)*n_mode+sys_ind,:),inf)<=...
             eps*s(t,sys_ind)];
     end
     Constraint = [Constraint sum(s(t,:))==1];
-    Constraint = [Constraint sum(X((t-1)*num_m+1:t*num_m,n+1:2*n),1)...
-        ==sum(X(t*num_m+1:(t+1)*num_m,1:n),1)];
+    Constraint = [Constraint sum(X((t-1)*n_mode+1:t*n_mode,n+1:2*n),1)...
+        ==sum(X(t*n_mode+1:(t+1)*n_mode,1:n),1)];
 end
 
 % Constraint for the last output equation
-for sys_ind = 1: num_m
+for sys_ind = 1: n_mode
     for j = 1:n_y   % constraints for output equation
-        Constraint = [Constraint sys(sys_ind).g(j,:)*[X((N-1)*num_m+ ...
-            sys_ind,n+1:2*n) e((N-1)*num_m+sys_ind,j)]'+ ...
-            s(N,sys_ind)*sys(sys_ind).q(j,:)*[u(:,N) y(j,N)]' ==0];
+        Constraint = [Constraint sys(sys_ind).g(j,:)*[X((T-1)*n_mode+ ...
+            sys_ind,n+1:2*n) e((T-1)*n_mode+sys_ind,j)]'+ ...
+            s(T,sys_ind)*sys(sys_ind).q(j,:)*[input(:,T) output(j,T)]'==0];
     end
-    Constraint = [Constraint norm(e((N-1)*num_m+sys_ind,:),inf)<= ...
-        eps*s(N,sys_ind)];
-    Constraint = [Constraint norm(X((N-1)*num_m+sys_ind,n+1:2*n),inf)<=...
-        M(sys_ind)*s(N,sys_ind)];
+    Constraint = [Constraint norm(e((T-1)*n_mode+sys_ind,:),inf)<= ...
+        eps*s(T,sys_ind)];
+    Constraint = [Constraint norm(X((T-1)*n_mode+sys_ind,n+1:2*n),inf)<=...
+        M(sys_ind)*s(T,sys_ind)];
 end
-Constraint = [Constraint sum(s(N,:))==1];
-
+Constraint = [Constraint sum(s(T,:))==1];
 
 %% Setting up Options
 options = sdpsettings('verbose',0,'solver',solver);
@@ -140,7 +187,6 @@ if strcmp(solver,'cplex')
         Decision = 'The model is not invalidated';
     end
 end
-
 if strcmp(solver, 'gurobi')
     if strcmp(sol.info, 'Infeasible problem (GUROBI-GUROBI)')
         Decision = 'The model is invalidated';
@@ -148,6 +194,3 @@ if strcmp(solver, 'gurobi')
         Decision = 'The model is not invalidated';
     end
 end
-
-
-
